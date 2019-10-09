6fe29d3b7891262437b460da60b3eb69e63041aa
Mikail KOCAK <mikail@mirumee.com>
2019-10-09 12:59:27 +0200

    Add comments support

--- build.sh
+++ build.sh
@@ -5,8 +5,17 @@
     exit 1
 }
 
-TARGET_REFS=$1
+UNCLEAN_TARGET_REFS=$1
 OUT_DIR_PATH=$2
+
+cleaned_target_refs=
+
+for refname in $UNCLEAN_TARGET_REFS; do
+    echo "${refname}" | grep -F . \
+        && refname="tags/${refname}" \
+        || refname="origin/${refname}"
+    cleaned_target_refs="${cleaned_target_refs} ${refname}"
+done
 
 BASE_CWD=$PWD
 HERE=$(realpath $(dirname $0))
@@ -16,28 +25,46 @@ GRAPHQL_OUT_PATH="$(mktemp).graphql"
 
 export PATH=${PATH}:${HERE}/node_modules/.bin/
 
+failed() {
+    echo "Failed to $@" &2>1
+    exit 1
+}
+
 if [ ! -d saleor ]; then
     echo 'Cloning Saleor...'
     git clone https://github.com/mirumee/saleor.git saleor \
-        || { echo 'Failed to clone saleor' &2>1; exit 1; }
+        || failed clone saleor
 fi
 
 cd saleor \
-    || { echo 'Failed to change dir to saleor' &2>1; exit 1; }
+    || failed change dir to saleor
 
 echo 'Fetching remote...'
 git fetch origin;
+cd ..
 
 echo 'Generating docs...'
-for refname in $TARGET_REFS; do
+for refname in ${cleaned_target_refs}; do
     echo -e "\t- ${refname}..."
-    (git show origin/${refname}:${GRAPHQL_SRC_PATH} > $GRAPHQL_OUT_PATH \
-        || { echo "Failed to generate $refname" &2>1; exit 1; }
+
+    cd saleor \
+        || failed change dir to saleor
+
+    git checkout ${refname} -b $(cat /proc/sys/kernel/random/uuid) > /dev/null \
+        || failed checkout to "${refname}"
+
+    pip install -r requirements.txt > /dev/null \
+        || failed install saleor
+
+    SECRET_KEY=blah python manage.py get_graphql_schema > ${GRAPHQL_OUT_PATH} \
+        || failed generate GraphQL schema
+
      cd $BASE_CWD \
         && graphdoc \
-                -s $GRAPHQL_OUT_PATH \
+                -t ./template/slds \
+                -s ${GRAPHQL_OUT_PATH} \
                 -o "${OUT_DIR_PATH}/${refname}" \
-                --force > /dev/null)
+                --force > /dev/null
     [ $? -ne 0 ] && exit 1
 done
 
@@ -56,15 +83,15 @@ cat > ${OUT_DIR_PATH}/index.html <<EOF
     <body class='container'>
        <h2 class='mt-4 mb-4'>Saleor GraphQL documentations</h2>
        <ul class='list-group'>
-          $(cd saleor; for refname in $TARGET_REFS; do
-            rev=$(git rev-parse --short origin/${refname})
-            date=$(git log -1 --format="%at" origin/${refname} | xargs -I{} date -d @{} +%Y/%m/%d\ %H:%M:%S\ %Z)
+          $(cd saleor; for refname in ${cleaned_target_refs}; do
+            rev=$(git rev-parse --short ${refname})
+            date=$(git log -1 --format="%at" ${refname} | xargs -I{} date -d @{} +%Y/%m/%d\ %H:%M:%S\ %Z)
             echo "<li class='list-group-item list-group-item-action \
                              flex-column align-items-start'> \
                     <a class='d-block text-dark' href='${refname}/index.html'> \
-                      <h4 class='mb-1'>${refname} branch</h4> \
+                      <h4 class='mb-1'>$(echo ${refname} | sed 's/^[a-z]*\///g') version</h4> \
                       <blockquote class='mb-1 ml-4 pl-2' style='border-left: 4px solid #CCC'> \
-                        $(git log --format=%B -n 1 origin/${refname} | sed 's/$/<br>/') \
+                        $(git log --format=%B -n 1 ${refname} | sed 's/$/<br>/') \
                       </blockquote> \
                     </a> \
                     <small>$date on<a \
